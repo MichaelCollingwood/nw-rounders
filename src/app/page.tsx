e222ce7091be3117508392101e77fd8ae7314a12
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2024-04-09 22:23:13 +0100

    Make relevant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="p-24">
+      <div className="p-24 text-3xl">
         Neighbourhood Watch Rounders
       </div>
       <div className="flex flex-col gap-2 px-10">
